0905a3af751d96248238f365ac8d61aebcaea65f
hemshushivhare <93434872+hemshushivhare@users.noreply.github.com>
2024-07-05 00:31:09 +0530

    Initial commit

---
+++ components/social-links.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+            <a href="https://github.com/hemshushivhare" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/hemshu-shivhare-5927491ba/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
